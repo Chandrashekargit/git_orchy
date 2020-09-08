26de6422ab8c2ea1da36a8ddaba6f943e0d1fb9f
chandrashekargit <chandrashkr.dot@gmail.com>
2020-09-08 16:17:03 +0530

    reportsmodule xpaths

---
+++ Orchestron_pytest/xpath/reports_module_xpath.py
@@ -0,0 +1,9 @@
+# reports module
+reports_section = "//p[contains(text(), 'Reports')]"
+
+# severity section
+high_sev = "//div[@role='group']/div[@class='row']//div[@class='col-sm-6'][1]//span[@class='el-checkbox__inner']"
+med_sev = "//div[@role='group']/div[@class='row']//div[@class='col-sm-6'][2]//span[@class='el-checkbox__inner']"
+low_sev = "//div[@role='group']/div[@class='row']//div[@class='col-sm-6'][3]//span[@class='el-checkbox__inner']"
+info_sev = "//div[@role='group']/div[@class='row']//div[@class='col-sm-6'][4]//span[@class='el-checkbox__inner']"
+
