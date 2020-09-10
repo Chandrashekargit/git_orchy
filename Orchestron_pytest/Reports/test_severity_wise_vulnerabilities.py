0b522312e560fe6085ec6ad2b010422a930ee6db
chandrashekargit <chandrashkr.dot@gmail.com>
2020-09-10 12:42:55 +0530

    New TC added to check severity of ai

--- Orchestron_pytest/Reports/test_severity_wise_vulnerabilities.py
+++ Orchestron_pytest/Reports/test_severity_wise_vulnerabilities.py
@@ -16,10 +16,10 @@ sca_tools = ["/home/junaid/Downloads/results_supported_by_orchy/OWASP Dependency
 sca_names = ["OWASP Dependency", "snyk", "whitesource", "Retire", "npm", "snyk"]
 
 
-@mark.a
-class reports_severity_wise_Tests:
+@mark.check_sev_filter
+class reports_severity_count_Tests:
     """
-    This Testcase checks when all the affected instances are closed then vul should move to closed vulnerable section.
+    This Testcase checks the workflow of severity filter.
     """
     def test_create_app(self, driver):
         wait = WebDriverWait(driver, 20, poll_frequency=2, ignored_exceptions=[
@@ -42,52 +42,119 @@ class reports_severity_wise_Tests:
             # waits until the Loading symbol is invisible
             WebDriverWait(driver, 60).until(EC.invisibility_of_element((By.XPATH, "//div[@class='loading-background']")))
 
-    def test_goto_reports_section(self, driver):
+    def test_goto_reports_section(self, driver, app_name="//label[contains(text(), 'check reports')]/../..//span[@class='el-checkbox__inner']"):
         wait = WebDriverWait(driver, 20, poll_frequency=2, ignored_exceptions=[
             NoSuchElementException, ElementNotVisibleException, ElementClickInterceptedException])
 
         go_to_reports_section = wait.until(EC.element_to_be_clickable((By.XPATH, reports_section)))
         go_to_reports_section.click()
-
+        select_the_required_app = wait.until(EC.element_to_be_clickable((By.XPATH, app_name)))
+        select_the_required_app.click()
         unselect_all_severities = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@style='min-height: 100%;']//div[@class='col-6']//span[@class='el-checkbox__inner']")))
         unselect_all_severities.click()
 
         for i in [high_sev, med_sev, low_sev, info_sev]:
-            select_high = wait.until(EC.element_to_be_clickable((By.XPATH, i)))
-            select_high.click()
+            stop_till_spinner_is_invisible(driver)
+            time.sleep(2)
+            select_severity = wait.until(EC.element_to_be_clickable((By.XPATH, i)))
+            select_severity.click()
 
-            # verify if all other severity count is zero
+            # verify if all other severity count is zero apart from the selected one.
             stop_till_spinner_is_invisible(driver)
             time.sleep(2)
             if i == high_sev:
-                medium_count = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='medium-round']")))
-                low_count = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='low-round']")))
-                info_count = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='info-round']")))
-                assert medium_count.text == "0" and low_count.text == "0" and info_count.text == "0"
-                high_count = wait.until(EC.presence_of_element_located((By.XPATH, high_sev)))
-                high_count.click()
+                check_medium_count = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='medium-round']")))
+                check_low_count = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='low-round']")))
+                check_info_count = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='info-round']")))
+                assert check_medium_count.text == "0" and check_low_count.text == "0" and check_info_count.text == "0"
+                unselect_high_sev = wait.until(EC.presence_of_element_located((By.XPATH, high_sev)))
+                unselect_high_sev.click()
             elif i == med_sev:
                 stop_till_spinner_is_invisible(driver)
-                high_count = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='high-round']")))
-                low_count = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='low-round']")))
-                info_count = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='info-round']")))
-                assert high_count.text == "0" and low_count.text == "0" and info_count.text == "0"
-                medium_count = wait.until(EC.presence_of_element_located((By.XPATH, med_sev)))
-                medium_count.click()
+                check_high_count = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='high-round']")))
+                check_low_count = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='low-round']")))
+                check_info_count = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='info-round']")))
+                assert check_high_count.text == "0" and check_low_count.text == "0" and check_info_count.text == "0"
+                unselect_medium_sev = wait.until(EC.presence_of_element_located((By.XPATH, med_sev)))
+                unselect_medium_sev.click()
             elif i == low_sev:
                 stop_till_spinner_is_invisible(driver)
-                high_count = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='high-round']")))
-                medium_count = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='medium-round']")))
-                info_count = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='info-round']")))
-                assert high_count.text == "0" and medium_count.text == "0" and info_count.text == "0"
-                low_count = wait.until(EC.presence_of_element_located((By.XPATH, low_sev)))
-                low_count.click()
+                check_high_count = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='high-round']")))
+                check_medium_count = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='medium-round']")))
+                check_info_count = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='info-round']")))
+                assert check_high_count.text == "0" and check_medium_count.text == "0" and check_info_count.text == "0"
+                unselect_low_sev = wait.until(EC.presence_of_element_located((By.XPATH, low_sev)))
+                unselect_low_sev.click()
             else:
                 stop_till_spinner_is_invisible(driver)
-                high_count = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='high-round']")))
-                medium_count = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='medium-round']")))
-                low_count = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='low-round']")))
-                assert high_count.text == "0" and medium_count.text == "0" and low_count.text == "0"
+                check_high_count = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='high-round']")))
+                check_medium_count = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='medium-round']")))
+                check_low_count = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='low-round']")))
+                assert check_high_count.text == "0" and check_medium_count.text == "0" and check_low_count.text == "0"
+                unselect_info_sev = wait.until(EC.presence_of_element_located((By.XPATH, info_sev)))
+                unselect_info_sev.click()
+
+    def test_affected_instance_sev(self, driver):
+        wait = WebDriverWait(driver, 20, poll_frequency=2, ignored_exceptions=[
+            NoSuchElementException, ElementNotVisibleException, ElementClickInterceptedException])
+
+        for (a, b) in zip([high_sev, med_sev, low_sev, info_sev], [1, 2, 3, 4]):
+            stop_till_spinner_is_invisible(driver)
+            time.sleep(2)
+            select_severity = wait.until(EC.element_to_be_clickable((By.XPATH, a)))
+            select_severity.click()
+
+            # verify if all other severity count is zero apart from the selected one.
+            stop_till_spinner_is_invisible(driver)
+            time.sleep(2)
+
+            try:
+                stop_till_spinner_is_invisible(driver)
+                move_to_last_page = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@aria-label='Go to last page']")))
+                move_to_last_page.click()
+
+                get_the_max_len_of_pagination = wait.until(EC.presence_of_element_located((By.XPATH, "//li[@class='page-item active']"))).text
+                stop_till_spinner_is_invisible(driver)
+                move_to_first_page = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@aria-label='Go to first page']")))
+                move_to_first_page.click()
+
+                for j in range(1, int(get_the_max_len_of_pagination)+1):
+                    select_pagination = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@aria-label='Go to page "+str(j)+"']")))
+                    select_pagination.click()
+                    time.sleep(2)
+                    severity_selected = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@role='group']/div[@class='row']//div[@class='col-sm-6']["+str(b)+"]/label//span[2]/label")))
+                    get_the_count_of_number_of_vul = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='card mb-1']")))
+                    for k in range(1, len(get_the_count_of_number_of_vul)+1):
+                        severity_of_vul = wait.until(EC.presence_of_element_located((By.XPATH,
+                                         "//div[@class='card mb-1']["+str(k)+"]"
+                                         "//span[@class='badge high move_right orchy_font_family  orchy_font_sm badge-secondary' "
+                                         "or @class='badge medium move_right orchy_font_family  orchy_font_sm badge-secondary' "
+                                         "or @class='badge low move_right orchy_font_family  orchy_font_sm badge-secondary' "
+                                         "or @class='badge info move_right orchy_font_family  orchy_font_sm badge-secondary']")))
+                        print(severity_of_vul.text)
+                        assert severity_of_vul.text == severity_selected.text
+            except TimeoutException:
+                stop_till_spinner_is_invisible(driver)
+                severity_selected = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@role='group']/div[@class='row']//div[@class='col-sm-6']["+str(b)+"]/label//span[2]/label")))
+                try:
+                    get_the_count_of_number_of_vul = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='card mb-1']")))
+
+                    for l in range(1, len(get_the_count_of_number_of_vul)+1):
+                        stop_till_spinner_is_invisible(driver)
+                        severity_of_vul = wait.until(EC.presence_of_element_located((By.XPATH,
+                                         "//div[@class='card mb-1']["+str(l)+"]"
+                                         "//span[@class='badge high move_right orchy_font_family  orchy_font_sm badge-secondary' "
+                                         "or @class='badge medium move_right orchy_font_family  orchy_font_sm badge-secondary' "
+                                         "or @class='badge low move_right orchy_font_family  orchy_font_sm badge-secondary' "
+                                         "or @class='badge info move_right orchy_font_family  orchy_font_sm badge-secondary']")))
+                        print(severity_of_vul.text)
+                        assert severity_of_vul.text == severity_selected.text
+                except TimeoutException:
+                    print("No vulnerabilities present under: ", severity_selected.text)
+                    
+            stop_till_spinner_is_invisible(driver)
+            unselect_severity = wait.until(EC.element_to_be_clickable((By.XPATH, a)))
+            unselect_severity.click()
 
     def test_delete_app(self, driver):
         delete_app(driver, application="//label[contains(text(),'check reports')]")
