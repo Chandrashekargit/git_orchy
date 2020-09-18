34d8f913adbf772fe330806b938f1886cdb04c3c
chandrashekargit <chandrashkr.dot@gmail.com>
2020-09-18 10:49:10 +0530

    added new xpaths

--- Orchestron_pytest/xpath/reports_module_xpath.py
+++ Orchestron_pytest/xpath/reports_module_xpath.py
@@ -7,3 +7,8 @@ med_sev = "//div[@role='group']/div[@class='row']//div[@class='col-sm-6'][2]//sp
 low_sev = "//div[@role='group']/div[@class='row']//div[@class='col-sm-6'][3]//span[@class='el-checkbox__inner']"
 info_sev = "//div[@role='group']/div[@class='row']//div[@class='col-sm-6'][4]//span[@class='el-checkbox__inner']"
 
+# report type xpaths
+exec_summary = "//input[@value='execSummary']/../span"
+detailed_report = "//input[@value='detailedReport']/../span"
+comparison = "//input[@value='Comparison']/../span"
+daterange_comparison = "//input[@value='dateRangeComparison']/../span"
