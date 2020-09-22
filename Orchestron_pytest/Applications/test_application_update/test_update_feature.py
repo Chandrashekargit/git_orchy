11d14b39e05cd641350b0b0ab23ec5ed59cde3ff
chandrashekargit <chandrashkr.dot@gmail.com>
2020-09-22 12:53:48 +0530

    code enhancements

--- Orchestron_pytest/Applications/test_application_update/test_update_feature.py
+++ Orchestron_pytest/Applications/test_application_update/test_update_feature.py
@@ -1,18 +1,25 @@
 from Applications.test_create_applications.create_app import *
 from Applications.test_delete_application.delete_app import *
+from pytest import mark
 
+
+@mark.test_update_feature
 class UpdateFeatureTests:
     def test_create_app(self, driver):
-        create_apps(driver, application_name="update feature", url="https://spotify.com/")
-
-        # waits until the submit is invisible
-        WebDriverWait(driver, 60).until(EC.invisibility_of_element((By.XPATH, upload_results_submit)))
-        # waits until the Loading symbol is invisible
-        WebDriverWait(driver, 60).until(EC.invisibility_of_element((By.XPATH, "//div[@class='loading-background']")))
-
-    def test_application_update_feature(self, driver, app_name="//label[contains(text(),'update feature')]", updated_app_name="New updated name", updated_url="https://checkupdatefeature.com"):
         wait = WebDriverWait(driver, 20, poll_frequency=2, ignored_exceptions=[
             NoSuchElementException, ElementNotVisibleException, TimeoutException, ElementClickInterceptedException])
+
+        create_apps(driver, application_name="update feature", url="https://spotify.com/")
+
+        stop_till_spinner_is_invisible(driver)
+        success_msg = wait.until(EC.visibility_of_element_located((By.XPATH, success_msg_for_app_created)))
+        assert success_msg.text == "Application has been created successfully!"
+        wait.until(EC.invisibility_of_element((By.XPATH, success_msg_for_app_created)))
+
+    def test_application_update_feature(self, driver, app_name="//label[contains(text(),'update feature')]",
+                updated_app_name="New updated name", updated_url="https://checkupdatefeature.com", platform="Ruby"):
+        wait = WebDriverWait(driver, 20, poll_frequency=2, ignored_exceptions=[
+             NoSuchElementException, ElementNotVisibleException, TimeoutException, ElementClickInterceptedException])
 
         stop_till_spinner_is_invisible(driver)
         applicationTab = wait.until(EC.element_to_be_clickable((By.XPATH, application_tab)))
@@ -22,33 +29,50 @@ class UpdateFeatureTests:
         click_on_individual_app = wait.until(EC.element_to_be_clickable((By.XPATH, app_name)))
         click_on_individual_app.click()
 
+        stop_till_spinner_is_invisible(driver)
+        stop_till_spinner_is_invisible(driver)
         action = wait.until(EC.element_to_be_clickable((By.XPATH, action_dropdown)))
         action.click()
 
         select_update_option = wait.until(EC.element_to_be_clickable((By.XPATH, update)))
         select_update_option.click()
 
+        stop_till_spinner_is_invisible(driver)
         update_name = wait.until(EC.element_to_be_clickable((By.XPATH, update_app_name)))
         update_name.clear()
         update_name.send_keys(updated_app_name)
+
+        stop_till_spinner_is_invisible(driver)
+        change_platformtype = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@class='row my-1'][2]//i[@class='el-tag__close el-icon-close']")))
+        change_platformtype.click()
+        platform_type = wait.until(EC.presence_of_element_located((By.XPATH, app_platform_type)))
+        platform_type.send_keys(platform)
+        platform_type.send_keys(Keys.ARROW_DOWN)
+        platform_type.send_keys(Keys.ENTER)
 
         url_field = wait.until(EC.presence_of_element_located((By.XPATH, app_url)))
         url_field.clear()
         url_field.send_keys(updated_url)
 
+        team = wait.until(EC.presence_of_element_located((By.XPATH, app_team)))
+        team.click()
+
         submit = wait.until(EC.element_to_be_clickable((By.XPATH, app_submit)))
         submit.click()
 
         stop_till_spinner_is_invisible(driver)
-        wait.until(EC.visibility_of_element_located((By.XPATH, "//p[.='Application has been updated successfully!']")))
-        wait.until(EC.invisibility_of_element_located((By.XPATH, "//p[.='Application has been updated successfully!']")))
+        wait.until(EC.visibility_of_element_located((By.XPATH, update_success_msg)))
+        wait.until(EC.invisibility_of_element_located((By.XPATH, update_success_msg)))
 
         # assert the updated info
         check_app_name = wait.until(EC.visibility_of_element_located((By.XPATH, "//div[@class='container-fluid']//div[@class='col-5'][1]//tr[1]/td[2]")))
-        assert check_app_name.text == ":"+updated_app_name
+        assert check_app_name.text == ": "+updated_app_name
 
         check_url = wait.until(EC.visibility_of_element_located((By.XPATH, "//div[@class='container-fluid']//div[@class='col-5'][1]//tr[2]/td[2]")))
-        assert check_url.text == ":"+updated_url
+        assert check_url.text == ": "+updated_url
+
+        check_platform = wait.until(EC.visibility_of_element_located((By.XPATH, "//div[@class='container-fluid']//div[@class='col-5'][1]//tr[3]/td[2]")))
+        assert check_platform.text == ": "+platform
 
     def test_delete_app(self, driver):
-        delete_app(driver, application="//label[contains(text(),'update feature')]")
+        delete_app(driver, application="//label[contains(text(),'New updated name')]")
--- Orchestron_pytest/Applications/test_application_update/test_warning_msg_while_updating_application.py
+++ Orchestron_pytest/Applications/test_application_update/test_warning_msg_while_updating_application.py
@@ -1,59 +1,72 @@
-import time
 from pytest import mark
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium import webdriver
-import pytest
-from selenium.common.exceptions import *
-from selenium.common.exceptions import TimeoutException
-from xpath.Application_module_xpath import *
 from Applications.test_delete_application.delete_app import delete_app
+from Applications.test_create_applications.create_app import *
 
 
-@mark.update_app_and_check_all_warning_msgs
-def test_warning_messaege_while_updating_application(driver, create_app):
-    wait = WebDriverWait(driver, 10, poll_frequency=2, ignored_exceptions=[ElementClickInterceptedException,
-                                                                           ElementNotInteractableException,
-                                                                           TimeoutException,
-                                                                           ElementNotVisibleException])
+@mark.update_feature_wrng_msgs
+class UpdateFeatureWarningMessagesTests:
+    def test_create_app(self, driver):
+        wait = WebDriverWait(driver, 10, poll_frequency=1)
+        for i in ["update_warning_messages", '2']:
+            create_apps(driver, application_name=i, url="http://demo.com")
+            stop_till_spinner_is_invisible(driver)
+            success_msg = wait.until(EC.visibility_of_element_located((By.XPATH, success_msg_for_app_created)))
+            assert success_msg.text == "Application has been created successfully!"
+            wait.until(EC.invisibility_of_element((By.XPATH, success_msg_for_app_created)))
 
-    search_tab = wait.until(EC.visibility_of_element_located((By.XPATH, search)))
-    search_tab.send_keys("DemoApplication")
+    def test_warning_messaege_while_updating_application(self, driver):
+        wait = WebDriverWait(driver, 10, poll_frequency=2, ignored_exceptions=[ElementClickInterceptedException,
+                                   TimeoutException, ElementNotVisibleException])
 
-    select_individual_app = wait.until(EC.element_to_be_clickable((By.XPATH, "//label[text()='DemoApplication']")))
-    select_individual_app.click()
+        # search_tab = wait.until(EC.visibility_of_element_located((By.XPATH, search)))
+        # search_tab.send_keys("DemoApplication")
 
-    action = wait.until(EC.element_to_be_clickable((By.XPATH, action_dropdown)))
-    action.click()
+        stop_till_spinner_is_invisible(driver)
+        applicationTab = wait.until(EC.element_to_be_clickable((By.XPATH, application_tab)))
+        applicationTab.click()
 
-    update_app = wait.until(EC.element_to_be_clickable((By.XPATH, update)))
-    update_app.click()
+        stop_till_spinner_is_invisible(driver)
+        select_individual_app = wait.until(EC.element_to_be_clickable((By.XPATH, "//label[text()='update_warning_messages']")))
+        select_individual_app.click()
 
-    update_name = wait.until(EC.element_to_be_clickable((By.XPATH, update_app_name)))
-    update_name.clear()
-    update_name.send_keys("Demo " * 10)
+        stop_till_spinner_is_invisible(driver)
+        time.sleep(2)
+        action = wait.until(EC.element_to_be_clickable((By.XPATH, action_dropdown)))
+        action.click()
+        update_app = wait.until(EC.element_to_be_clickable((By.XPATH, update)))
+        update_app.click()
 
-    name_warning_msg = wait.until(EC.element_to_be_clickable((By.XPATH, update_name_warning_msg))).text
-    assert "* Ensure this field has no more than 50 characters." in name_warning_msg
+        stop_till_spinner_is_invisible(driver)
+        update_name = wait.until(EC.element_to_be_clickable((By.XPATH, update_app_name)))
+        update_name.clear()
+        update_name.send_keys("Demo " * 10)
 
-    update_URL = wait.until(EC.element_to_be_clickable((By.XPATH, update_url)))
-    update_URL.clear()
-    update_URL.send_keys("http://!1!!111.com")
+        name_warning_msg = wait.until(EC.visibility_of_element_located((By.XPATH, update_name_warning_msg))).text
+        assert name_warning_msg == "* Ensure this field has no more than 50 characters."
 
-    submit = wait.until(EC.element_to_be_clickable((By.XPATH, update_submit_btn)))
-    submit.click()
-    time.sleep(1)
+        update_name = wait.until(EC.element_to_be_clickable((By.XPATH, update_app_name)))
+        update_name.clear()
+        update_name.send_keys(2)
 
-    url_warning_msg = wait.until(EC.element_to_be_clickable((By.XPATH, update_url_warning_msg))).text
-    assert "* Enter a valid URL." in url_warning_msg
-    time.sleep(1)
-    close_popup = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@class='close']")))
-    close_popup.click()
-    driver.refresh()
-    time.sleep(2)
+        update_URL = wait.until(EC.element_to_be_clickable((By.XPATH, update_url)))
+        update_URL.clear()
+        update_URL.send_keys("http://!1!!1.com")
 
-    # Delete the application
-    delete_app(driver, application="//label[contains(text(),'DemoApplication')]")  # calling the function 'delete_app'
+        submit = wait.until(EC.element_to_be_clickable((By.XPATH, update_submit_btn)))
+        submit.click()
 
+        stop_till_spinner_is_invisible(driver)
+        url_warning_msg = wait.until(EC.element_to_be_clickable((By.XPATH, update_url_warning_msg))).text
+        assert "* Enter a valid URL." in url_warning_msg
+
+        existing_app_name_wrng_msg = wait.until(EC.visibility_of_element_located((By.XPATH, existing_application_warning_msg)))
+        assert existing_app_name_wrng_msg.text == '* Application with this name already exists.'
+
+        stop_till_spinner_is_invisible(driver)
+        close_popup = wait.until(EC.element_to_be_clickable((By.XPATH, app_pop_up_close_btn)))
+        close_popup.click()
+        driver.refresh()
+
+    def test_delete_app(self, driver):
+        for i in ["//label[text()='update_warning_messages']", "//label[contains(text(),'2')]"]:
+            delete_app(driver, application=i)
